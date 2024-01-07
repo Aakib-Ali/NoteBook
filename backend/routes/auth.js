c7eeeb09fd990a549deabd187dfec10a468c2aa4
Aakib-Ali <aakibali449@gmail.com>
2024-01-07 14:54:19 +0530

    token created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User  = require('../models/User');
-const {body,validationResult} = require('express-validator');
-
-//to use cryptography
+const {body,validationResult} = require('express-validator');//to use cryptography
 const bcrypt = require('bcryptjs');
+const jwt=require('jsonwebtoken');//import jsonwebtoken
 
-
+const JWT_SECRET='Thereisnothingwithout$Allah$';
 //create a user using: POST- "/api/auth/". Dosen't require auth create user without login
 router.post('/createuser',[
     body('name', "Enter valid name").isLength({min:3}),
@@ -36,7 +35,20 @@ router.post('/createuser',[
         email: req.body.email,
         password: secPassword
     });
-    res.send({user});
+    
+    //create authentication token to verify the user
+    const data= {                   //take id of user from data
+        user:{
+            id:user.id
+        } 
+    }
+
+    //jwt use
+    const authToken =jwt.sign(data ,JWT_SECRET);
+    res.send({authToken})
+
+
+    // res.send({user});
 }catch(error){
     console.log(error.message);
     res.status(500).send("Some error occured");
