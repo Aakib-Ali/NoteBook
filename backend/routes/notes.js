1a0acdf57b564e6f568ea31181b5327c640ccca2
Aakib-Ali <aakibali449@gmail.com>
2024-01-08 16:53:48 +0530

    delete and update api

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -15,6 +15,7 @@ router.post('/addnotes', [
         if(!error.isEmpty()){
             return res.status(400).send({errors: error.array()});
         }
+        //destructuring 
         const { title,description, teg}=req.body;
         const note= new Note({
             title,description,teg, user:req.user.id
@@ -37,6 +38,87 @@ router.get('/getnotes', fetchuser, async (req, res) => {
         console.error(error.message);
         res.status(500).send('Internal Server Error');
     }
+});
+
+//Route 3: Update existing notes using :id param and :put /api/notes/update:id
+router.put("/update/:id", fetchuser ,async (req, res) => {
+
+    try{
+        const error=validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).send({errors: error.array()});
+        }
+        const {title ,description,tag}=req.body;
+        const newNote= {};
+        if(title) newNote.title=title;
+        if(description) newNote.description=description;
+        if(tag) newNote.tag=tag;
+        
+        //find notes to be updated
+        let note= await Note.findById(req.params.id);
+        if (!note) return res.status(404).send("Not found");
+
+        // Allow update only for authorized person
+        if(note.user.toString()!=req.user.id) 
+            return res.status(401).send("Not allowed");
+        note= await Note.findByIdAndUpdate(req.params.id,{$set:newNote}, { new: true});
+        res.send(note);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Error Occurs");
+      }
+
+});
+//Route 3: Update existing notes using :id param and :put /api/notes/update:id
+router.put("/update/:id", fetchuser ,async (req, res) => {
+
+    try{
+        const error=validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).send({errors: error.array()});
+        }
+        const {title ,description,tag}=req.body;
+        const newNote= {};
+        if(title) newNote.title=title;
+        if(description) newNote.description=description;
+        if(tag) newNote.tag=tag;
+        
+        //find notes to be updated
+        let note= await Note.findById(req.params.id);
+        if (!note) return res.status(404).send("Not found");
+
+        // Allow update only for authorized person
+        if(note.user.toString()!=req.user.id) 
+            return res.status(401).send("Not allowed");
+        note= await Note.findByIdAndUpdate(req.params.id,{$set:newNote}, { new: true});
+        res.send(note);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Error Occurs");
+      }
+
+});
+//Route 4: delet existing notes using :id param and Using : Delet /api/notes/delet:id
+router.delete("/delete/:id", fetchuser ,async (req, res) => {
+
+    try{
+        const error=validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).send({errors: error.array()});
+        }
+        //find notes to be updated
+        let note= await Note.findById(req.params.id);
+        if (!note) return res.status(404).send("Not found");
+        // Allow delet only for authorized person
+        if(note.user.toString()!=req.user.id) 
+            return res.status(401).send("Not allowed");
+        note= await Note.findByIdAndDelete(req.params.id);
+        res.json({"Success": "Succesfully Deleted", note:note});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Error Occurs");
+      }
+
 });
 
 module.exports=router;
